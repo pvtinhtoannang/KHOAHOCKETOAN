e183bf62bd4f5abe6fbcd234878c11c0993887c6
Minh Nhut <nhutfs@gmail.com>
2020-04-16 18:13:32 +0700

    Update AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,11 +6,6 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
     function getAdminDashboard()
     {
         return view('admin.dashboard.dashboard-home');
