7bf965e3257d74f6a46b784716dac1682e5ba4f3
Minh Nhut <nhutfs@gmail.com>
2020-04-16 18:14:03 +0700

    Update category.blade.php

--- resources/views/admin/category/category.blade.php
+++ resources/views/admin/category/category.blade.php
@@ -7,35 +7,36 @@
             <div class="kt-portlet kt-portlet--mobile">
                 <div class="kt-portlet__body">
                     <h2 class="template-sub-title">Thêm chuyên mục</h2>
-                    <form class="kt-form" id="category">
+                    <form class="kt-form" id="category" method="post">
                         <div class="form-group">
                             <label for="category-name">Tên</label>
-                            <input type="text" class="form-control" id="category-name" name="category_name">
+                            <input type="text" class="form-control" id="category-name" name="category_name" required>
                             <span class="form-text text-muted">Tên riêng sẽ hiển thị trên trang mạng của bạn.</span>
                         </div>
                         <div class="form-group">
                             <label for="category-slug">Chuỗi cho đường dẫn tĩnh</label>
-                            <input type="text" class="form-control" id="category-slug" name="category_slug">
+                            <input type="text" class="form-control" id="category-slug" name="category_slug" required>
                             <span class="form-text text-muted">Chuỗi cho đường dẫn tĩnh là phiên bản của tên hợp chuẩn với Đường dẫn (URL). Chuỗi này bao gồm chữ cái thường, số và dấu gạch ngang (-).</span>
                         </div>
                         <div class="form-group">
                             <label for="category-parent">Chuyên mục hiện tại</label>
                             <select class="form-control" id="category-parent" name="category_parent">
-                                <option>Trống</option>
-                                <option>Chuyên mục 1</option>
-                                <option>Chuyên mục 2</option>
-                                <option>Chuyên mục 3</option>
-                                <option>Chuyên mục 4</option>
+                                <option value="0" selected>Trống</option>
+                                @foreach($categoryData as $categoryValue)
+                                    <option value="{{$categoryValue['term_id']}}">{{$categoryValue['name']}}</option>
+                                @endforeach
                             </select>
                             <span class="form-text text-muted">Chuyên mục khác với thẻ, bạn có thể sử dụng nhiều cấp chuyên mục. Ví dụ: Trong chuyên mục nhạc, bạn có chuyên mục con là nhạc Pop, nhạc Jazz. Việc này hoàn toàn là tùy theo ý bạn.</span>
                         </div>
                         <div class="form-group">
                             <label for="category-description">Mô tả</label>
-                            <textarea class="form-control" id="category-description" name="category_description" rows="5"></textarea>
+                            <textarea class="form-control" id="category-description" name="category_description"
+                                      rows="5"></textarea>
                         </div>
                         <div class="kt-form__actions">
                             <button type="submit" class="btn btn-primary">Thêm chuyên mục</button>
                         </div>
+                        {{ csrf_field() }}
                     </form>
                 </div>
             </div>
@@ -48,13 +49,14 @@
                         <thead>
                         <tr>
                             <th>Tên</th>
+                            <th>Mô tả</th>
                             <th>Chuỗi cho đường dẫn tĩnh</th>
                         </tr>
                         </thead>
                         <tbody>
-                        @for($i = 0; $i <= 10; $i++)
+                        @foreach($categoryData as $categoryValue)
                             <tr>
-                                <td class="kt-font-bold">Tiêu đề bài viết
+                                <td class="kt-font-bold">{{$categoryValue['name']}}
                                     <div class="nowrap row-actions">
                                         <a href="#" class="btn btn-sm btn-clean btn-icon btn-icon-md" title="View">
                                             <i class="la la-eye"></i>
@@ -67,9 +69,16 @@
                                         </a>
                                     </div>
                                 </td>
-                                <td>chuyen-muc</td>
+                                <td>
+                                    @if($categoryValue['description'] == '')
+                                        {{'Không có mô tả'}}
+                                    @else
+                                        {{$categoryValue['description']}}
+                                    @endif
+                                </td>
+                                <td>{{$categoryValue['slug']}}</td>
                             </tr>
-                        @endfor
+                        @endforeach
                         </tbody>
                     </table>
 
