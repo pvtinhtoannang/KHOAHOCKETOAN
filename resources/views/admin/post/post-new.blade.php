c0347c498fb45776430d81403bfda5fbc1a67b02
Minh Nhut <nhutfs@gmail.com>
2020-04-18 15:24:44 +0700

    Update post-new.blade.php

--- resources/views/admin/post/post-new.blade.php
+++ resources/views/admin/post/post-new.blade.php
@@ -2,14 +2,31 @@
 @section('title', 'Thêm bài viết')
 @section('content')
     <h1 class="template-title">Thêm bài viết</h1>
-    <form class="kt-form" id="post">
+    <form class="kt-form" id="post" method="post">
         <div class="row">
             <div class="col-md-9">
                 <div class="form-group">
-                    <input type="text" name="post_title" id="title" class="form-control" placeholder="Thêm tiêu đề">
+                    <input type="text" name="post_title" id="post-title" class="form-control" placeholder="Thêm tiêu đề">
+                </div>
+                <div class="form-group row post-link-row">
+                    <label for="example-text-input" class="col-form-label">Đường dẫn tĩnh: </label>
+                    <span class="post-link">{{url('/')}}/</span>
+                    <div class="col-6">
+                        <input class="form-control" type="text" id="post-name" name="post_name">
+                    </div>
                 </div>
                 <div class="form-group">
-                    <textarea class="wysiwyg-editor" id="mytextarea" name="mytextarea"></textarea>
+                    <textarea class="summernote-post-content" id="post_content" name="post_content"></textarea>
+                </div>
+                <div class="kt-portlet">
+                    <div class="kt-portlet__head kt-bg-primary">
+                        <div class="kt-portlet__head-label">
+                            <h3 class="kt-font-bolder kt-portlet__head-title kt-font-light">Mô tả ngắn</h3>
+                        </div>
+                    </div>
+                    <div class="kt-portlet__body">
+                        <textarea class="form-control" id="excerpt" name="excerpt" rows="4"></textarea>
+                    </div>
                 </div>
             </div>
             <div class="col-md-3">
@@ -20,9 +37,17 @@
                         </div>
                     </div>
                     <div class="kt-portlet__body">
+                        <div class="form-group">
+                            <label for="post-status">Trạng thái</label>
+                            <select class="form-control" id="post-status" name="post_status">
+                                <option value="publish" selected>Đã xuất bản</option>
+                                <option value="pending">Chờ duyệt</option>
+                                <option value="draft">Bản nháp</option>
+                            </select>
+                        </div>
                     </div>
                     <div class="kt-portlet__foot kt-portlet__foot--sm kt-align-right">
-                        <a href="#" class="btn btn-primary">Đăng</a>
+                        <button class="btn btn-primary" type="submit">Đăng</button>
                     </div>
                 </div>
                 <div class="kt-portlet">
@@ -33,22 +58,13 @@
                     </div>
                     <div class="kt-portlet__body">
                         <div class="kt-checkbox-list">
-                            <label class="kt-checkbox">
-                                <input type="checkbox"> Chuyên mục 1
-                                <span></span>
-                            </label>
-                            <label class="kt-checkbox">
-                                <input type="checkbox"> Chuyên mục 2
-                                <span></span>
-                            </label>
-                            <label class="kt-checkbox">
-                                <input type="checkbox"> Chuyên mục 3
-                                <span></span>
-                            </label>
-                            <label class="kt-checkbox">
-                                <input type="checkbox"> Chuyên mục 4
-                                <span></span>
-                            </label>
+                            @foreach($categoryData as $categoryValue)
+                                <label class="kt-checkbox">
+                                    <input name="post_category[]" type="checkbox"
+                                           value="{{$categoryValue['term_id']}}"> {{$categoryValue['name']}}
+                                    <span></span>
+                                </label>
+                            @endforeach
                         </div>
                     </div>
                 </div>
@@ -59,7 +75,34 @@
                         </div>
                     </div>
                     <div class="kt-portlet__body">
-
+                        <div id="post-tag-repeater">
+                            <div class="form-group">
+                                <div data-repeater-list="">
+                                    <div data-repeater-item class="form-group row align-items-center">
+                                        <div class="col-md-10">
+                                            <input type="text" name="group[1] [post_tag]" class="form-control"
+                                                   placeholder="Thêm thẻ">
+                                        </div>
+                                        <div class="col-md-2">
+                                            <div data-repeater-delete=""
+                                                 class="btn btn-danger btn-elevate btn-circle btn-icon">
+																	<span>
+																		<i class="la la-trash-o"></i>
+																	</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <div data-repeater-create="" class="btn btn btn-sm btn-brand btn-pill">
+															<span>
+																<i class="la la-plus"></i>
+																<span>Thêm</span>
+															</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="kt-portlet">
@@ -81,5 +124,6 @@
                 </div>
             </div>
         </div>
+        {{ csrf_field() }}
     </form>
 @endsection
