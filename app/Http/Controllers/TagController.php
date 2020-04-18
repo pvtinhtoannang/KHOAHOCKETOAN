af5f9f4ea0120185647d43791a5eb090a508b9c2
pvtinhtoannang <49936064+pvtinhtoannang@users.noreply.github.com>
2020-04-18 09:04:24 +0700

    Update TagController.php

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -14,8 +14,7 @@ class TagController extends Controller
     {
         $this->taxonomy = 'post_tag';
         $this->term = new Terms();
-        $this->term_taxonomy = new TermTaxonomy();
-        tahy doi tag // chỗ này t sua ben t
+        $this->term_taxonomy = new TermTaxonomy(); 
 
     }
 
