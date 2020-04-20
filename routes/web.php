8f08ba64c14167c61c54d4c87d8c1eba4abf1860
Minh Nhut <nhutfs@gmail.com>
2020-04-20 16:32:26 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
     Route::get('/post-new', ['as' => 'GET_POST_NEW_ROUTE', 'uses' => 'PostController@getPostNew']);
     Route::post('/post-new', ['as' => 'POST_POST_NEW_ROUTE', 'uses' => 'PostController@postPostNew']);
 
-    Route::get('/post', ['as' => 'GET_POST_ALL_ROUTE', 'uses' => 'PostController@getPostALL']);
+    Route::get('/post', ['as' => 'GET_POST_ALL_ROUTE', 'uses' => 'PostController@getPostAll']);
 
     //category
     Route::get('/category', ['as' => 'GET_CATEGORY_ROUTE', 'uses' => 'CategoryController@getCategory']);
