fbc13c528fa93c2187cdd031090fc49610d2b685
Minh Nhut <nhutfs@gmail.com>
2020-04-20 13:43:44 +0700

    Update 2020_04_20_060746_create_posts_table.php

--- database/migrations/2020_04_20_060746_create_posts_table.php
+++ database/migrations/2020_04_20_060746_create_posts_table.php
@@ -15,13 +15,13 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->bigIncrements('ID');
-            $table->bigInteger('post_author')->unsigned();
+            $table->bigInteger('post_author')->unsigned()->default(0);
             $table->longText('post_content');
             $table->text('post_title');
             $table->text('post_excerpt');
-            $table->string('post_status', 20);
-            $table->string('post_name', 200);
-            $table->string('post_type', 20);
+            $table->string('post_status', 20)->default('publish');
+            $table->string('post_name', 200)->default('');
+            $table->string('post_type', 20)->default('post');
             $table->timestamps();
         });
     }
