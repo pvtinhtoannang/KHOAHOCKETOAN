8a22fcee21d1fef6f1a933fd7c43e3c5bd4f2974
Minh Nhut <nhutfs@gmail.com>
2020-04-21 14:51:36 +0700

    Update upload-new.blade.php

--- resources/views/admin/uploads/upload-new.blade.php
+++ resources/views/admin/uploads/upload-new.blade.php
@@ -1,17 +1,16 @@
 @extends('admin.dashboard.dashboard-master')
-@section('title', 'Thêm trang mới')
+@section('title', 'Tải lên')
 @section('content')
-    <h1 class="template-title">Thêm trang mới</h1>
-    <div class="form-group row">
-        <label class="col-form-label col-lg-3 col-sm-12">Multiple File Upload</label>
-        <div class="col-lg-4 col-md-9 col-sm-12">
-            <div class="kt-dropzone dropzone m-dropzone--primary" action="inc/api/dropzone/upload.php"
-                 id="m-dropzone-two">
-                <div class="kt-dropzone__msg dz-message needsclick">
-                    <h3 class="kt-dropzone__msg-title">Drop files here or click to upload.</h3>
-                    <span class="kt-dropzone__msg-desc">Upload up to 10 files</span>
-                </div>
-            </div>
+    <h1 class="template-title">Tải lên</h1>
+    <form method="post" action="{{route('POST_UPLOAD_NEW_ROUTE')}}"
+          enctype="multipart/form-data" class="dropzone" id="my-dropzone">
+        <div class="kt-dropzone__msg dz-message needsclick">
+            <h3 class="kt-dropzone__msg-title">Thả hoặc click vào đây để tải lên các tập tin</h3>
+            <span class="kt-dropzone__msg-desc">Kích thước tối đa 200MB</span>
         </div>
-    </div>
+        <div class="fallback">
+            <input type="file" name="file" multiple>
+        </div>
+        {{ csrf_field() }}
+    </form>
 @endsection
