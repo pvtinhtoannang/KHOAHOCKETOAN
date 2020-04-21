0812a900a46ba162f73b0b5e508d2f3d3c619696
Minh Nhut <nhutfs@gmail.com>
2020-04-21 09:55:56 +0700

    Update TermTaxonomy.php

--- app/TermTaxonomy.php
+++ app/TermTaxonomy.php
@@ -22,4 +22,11 @@ class TermTaxonomy extends Model
         return $this->create(['term_id' => $term_id, 'taxonomy' => $taxonomy, 'description' => $description, 'parent' => $parent, 'count' => 0]);
     }
 
+    function get_term_by_parent($parent = null, $taxonomy = null)
+    {
+        return $this->join('terms', 'terms.term_id', '=', 'term_taxonomy.term_id')
+            ->where('taxonomy', $taxonomy)
+            ->where('parent', $parent)
+            ->orderBy('term_taxonomy_id', 'asc')->get();
+    }
 }
