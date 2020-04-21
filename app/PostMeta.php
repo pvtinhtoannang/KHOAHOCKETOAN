768bd88cd25e5ed2d7b2056ad32a991da804ea15
Minh Nhut <nhutfs@gmail.com>
2020-04-21 14:51:03 +0700

    Update PostMeta.php

--- app/PostMeta.php
+++ app/PostMeta.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class PostMeta extends Model
 {
-    //
+    protected $table = 'postmeta';
+    public $timestamps = false;
+    protected $fillable = [
+        'meta_id',
+        'post_id',
+        'meta_key',
+        'meta_value'
+    ];
+
+    function addPostMeta($postMeta = null)
+    {
+        if (!empty($postMeta)) {
+            $this->insert($postMeta);
+        }
+    }
 }
