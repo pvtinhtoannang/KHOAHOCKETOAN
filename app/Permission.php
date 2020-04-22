9cb0dce5f8ad0f64791c57d7df47a2fb3381fc56
pvtinhtoannang <49936064+pvtinhtoannang@users.noreply.github.com>
2020-04-22 14:49:06 +0700

    Create Permission.php

---
+++ app/Permission.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Permission extends Model
+{
+
+    /**
+     * Relationship to Role Table
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function roles()
+    {
+        return $this->belongsToMany('App\Role', 'role_permission', 'permission_id', 'role_id');
+    }
+
+    /**
+     * @return \Illuminate\Database\Eloquent\Builder[]|\Illuminate\Database\Eloquent\Collection
+     */
+    public function getAllPermissionWithPaginate($paginate = 15)
+    {
+        return self::paginate($paginate);
+    }
+
+    public function getAllPermission(){
+        return self::get();
+    }
+}
