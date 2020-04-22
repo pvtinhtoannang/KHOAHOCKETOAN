9be3b5645e3a19d7078d17e042c29490424242e0
Minh Nhut <nhutfs@gmail.com>
2020-04-22 10:45:27 +0700

    Update upload-new.blade.php

--- resources/views/admin/uploads/upload-new.blade.php
+++ resources/views/admin/uploads/upload-new.blade.php
@@ -2,15 +2,5 @@
 @section('title', 'Tải lên')
 @section('content')
     <h1 class="template-title">Tải lên</h1>
-    <form method="post" action="{{route('POST_UPLOAD_NEW_ROUTE')}}"
-          enctype="multipart/form-data" class="dropzone" id="my-dropzone">
-        <div class="kt-dropzone__msg dz-message needsclick">
-            <h3 class="kt-dropzone__msg-title">Thả hoặc click vào đây để tải lên các tập tin</h3>
-            <span class="kt-dropzone__msg-desc">Kích thước tối đa 200MB</span>
-        </div>
-        <div class="fallback">
-            <input type="file" name="file" multiple>
-        </div>
-        {{ csrf_field() }}
-    </form>
+    @include('admin.components.upload-new')
 @endsection
