704bb31b694343a0b0d813e990b1afb64d036170
Minh Nhut <nhutfs@gmail.com>
2020-04-22 10:46:01 +0700

    Update AdminAjaxController.php

--- app/Http/Controllers/AdminAjaxController.php
+++ app/Http/Controllers/AdminAjaxController.php
@@ -3,17 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\PostMeta;
 use App\Terms;
 use Illuminate\Http\Request;
 
 class AdminAjaxController extends Controller
 {
-    private $term, $post;
+    private $term, $post, $post_meta;
 
     public function __construct()
     {
         $this->term = new Terms();
         $this->post = new Post();
+        $this->post_meta = new PostMeta();
     }
 
     public function index()
@@ -30,6 +32,15 @@ class AdminAjaxController extends Controller
     function checkPostName(Request $request)
     {
         echo json_encode($this->post->checkPostNameExists($request->post_name));
+        exit;
+    }
+
+    function getAttachedFile(Request $request)
+    {
+        $attached_meta = $this->post_meta->get_postmeta_by_meta_key($request->id, '_attached_file');
+        $uploads_url = url('/contents/uploads');
+        $file_url = $uploads_url . '/' . $attached_meta['meta_value'];
+        echo json_encode($file_url);
         exit;
     }
 }
